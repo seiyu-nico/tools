7e2d90836711482cd3f1f0ea3ae9a73914d174af
seiyu-nico <yuseiishii141+git@gmail.com>
2020-04-17 10:15:47 +0900

    リファクタリング

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -113,23 +113,13 @@ export default {
   mounted:function () {
     $(".sidebar-dropdown > a").click(function() {
       $(".sidebar-submenu").slideUp(200);
-      if (
-        $(this)
-          .parent()
-          .hasClass("active")
-      ) {
+      if ($(this).parent().hasClass("active")) {
         $(".sidebar-dropdown").removeClass("active");
-        $(this)
-          .parent()
-          .removeClass("active");
+        $(this).parent().removeClass("active");
       } else {
         $(".sidebar-dropdown").removeClass("active");
-        $(this)
-          .next(".sidebar-submenu")
-          .slideDown(200);
-        $(this)
-          .parent()
-          .addClass("active");
+        $(this).next(".sidebar-submenu").slideDown(200);
+        $(this).parent().addClass("active");
       }
     });
 
