a058df49953859252ffef98a352dae3a0ca6a284
seiyu-nico <yuseiisii141+git@gmail.com>
2020-07-21 12:46:49 +0900

    ページ遷移にデザイン追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,8 +4,10 @@
     <div class="page-wrapper chiller-theme toggled overflow-auto">
       <Sidebar></Sidebar>
       <main class="page-content">
-        <div class="container-fluid">
-          <RouterView />
+        <div class="container-fluid" id="router">
+          <transition>
+            <RouterView />
+          </transition>
         </div>
       </main>
     </div>
@@ -30,3 +32,28 @@ export default {
   },
 }
 </script>
+<style>
+#router .v-leave {
+  transform: translate(0, 0);
+  opacity: 1;
+}
+#router .v-leave-to {
+  transform: translate(0, 100px);
+  opacity: 0;
+}
+#router .v-leave-active {
+    transition: all 0.5s 0s ease-out;
+}
+
+/* 表示されていくスタイル */
+#router .v-enter {
+  transform: translate(0, -100px);
+  opacity: 0;
+}
+#router .v-enter-to {
+  opacity: 1;
+}
+#router .v-enter-active {
+  transition: all 0.5s 1s ease-out;
+}
+</style>
