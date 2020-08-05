60df3816a601bb1e823e0a4290d79ed5bac47eeb
seiyu-nico <yuseiisii141+git@gmail.com>
2020-08-05 12:57:54 +0900

    ツイッター用OGP追加

--- resources/js/components/StrCount/Index.vue
+++ resources/js/components/StrCount/Index.vue
@@ -48,6 +48,8 @@ export default {
       { property: 'og:url', content: 'https://tools.seiyu-nico.jp/str-count/' },
       { property: 'og:image', content: 'https://img.seiyu-nico.jp/icon/nico_icon.jpg' },
       { property: 'og:site_name', content: 'せぃゆーTools' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@seiyu_nico' },
     ]
   }
 }
