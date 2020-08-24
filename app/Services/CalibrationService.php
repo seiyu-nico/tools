47abaab80ed2ba1febb0d3369f8d13c448388a5f
seiyu-nico <yuseiisii141+git@gmail.com>
2020-08-24 09:00:27 +0900

    Guzzle使ってみた

--- app/Services/CalibrationService.php
+++ app/Services/CalibrationService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Log;
 use App\Services\ArrayService AS ARS;
+use GuzzleHttp\Client;
 
 class CalibrationService
 {
@@ -15,25 +16,24 @@ class CalibrationService
     public function __construct(ARS $ARS)
     {
         $this->ARS = $ARS;
+        // $this->GHC = $GHC;
     }
 
-    // public function callApi(ARS $AS, $sentence)
     public function callApi($sentence)
     {
-        $post = array(
-            'appid' => self::APPID,
-            'sentence' => $sentence,
-            'filter_group' => '',
-            'no_filter' => '',
-        );
-        $curl=curl_init(self::URL);
-        curl_setopt($curl, CURLOPT_POST, TRUE);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);  // オレオレ証明書対策
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);  // 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE); // Locationヘッダを追跡
-        $xml = curl_exec($curl);
+        $client = new Client();
+        $options = [
+            'form_params' => [
+                'appid' => self::APPID,
+                'sentence' => $sentence,
+                'filter_group' => '',
+                'no_filter' => '',
+            ]
+        ];
+            
+        $response = $client->request('POST', self::URL, $options);
+        $xml = $response->getBody();
+        
         $xml_to_array = json_decode(json_encode(simplexml_load_string($xml)), true);
         $multi_flag = $this->ARS->isMulti($xml_to_array['Result']);
         if (true == $multi_flag) {
